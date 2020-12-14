2c0a55abfa6b186caf90f6fb39f1365b041c7c60
Alvaro Hilado <jose.alvaro.hilado@gmail.com>
2020-12-14 14:33:51 +0800

    Adding password encoder and dashboard page

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,20 +1,30 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Bean // allows to get access outside of file
+	public PasswordEncoder getPasswordEncoder() {
+		// password encryption
+		return new BCryptPasswordEncoder();
+	}
 	
 	// Authentication creates the user
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		//inMemoryAuthentication creates user in memory. Not for Production
 		auth.inMemoryAuthentication()
+			.passwordEncoder(getPasswordEncoder())
 			.withUser("user@gmail.com")
-			.password("qwerty")
+			.password(getPasswordEncoder().encode("qwerty"))
 			.roles("USER");
 	}
 	
